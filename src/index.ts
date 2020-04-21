6d18c6d45cf6838626d2e4aaafc245f8447b661b
pabra <patrick+github-commit-2020@braune.xyz>
2020-04-21 15:18:24 +0200

    use function overloads to better tell apart using a reducer or not

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,96 @@
 import { Dispatch, useEffect, useState } from 'react';
 import { unstable_batchedUpdates as batch } from 'react-dom';
 
-export default function makeStore<
-  State,
-  Reducer,
-  ReducerAction = Reducer extends (state: State, action: infer T) => State
-    ? T
-    : never
->(
-  initialState: State,
-  reducer?: (state: State, action: ReducerAction) => State,
-) {
-  type NewStateFn = (oldState: State) => State;
+type ReducerAction<State, Reducer> = Reducer extends (
+  state: State,
+  action: infer T,
+) => State
+  ? T
+  : never;
 
+type Reducer<State> = (state: State, action: any) => State;
+
+type ReducerWithActionType<State, R extends Reducer<State>> = (
+  state: State,
+  action: ReducerAction<State, R>,
+) => State;
+
+type SetStateWithState<State> = (
+  arg: State | ((oldState: State) => State),
+) => void;
+
+type SetStateWithReducer<State, R extends Reducer<State>> = (
+  action: ReducerAction<State, R>,
+) => void;
+
+type UseStoreWithState<State> = () => [State, SetStateWithState<State>];
+
+type UseStoreWithReducer<State, R extends Reducer<State>> = () => [
+  State,
+  SetStateWithReducer<State, R>,
+];
+
+// first overload if only initial state is passed
+function makeStore<State>(initialState: State): UseStoreWithState<State>;
+
+// second overload with passed reducer
+function makeStore<State, R extends Reducer<State>>(
+  initialState: State,
+  reducer: R & ReducerWithActionType<State, R>,
+): UseStoreWithReducer<State, R>;
+
+// implementation
+function makeStore<State, R extends Reducer<State>>(
+  initialState: State,
+  reducer?: R & ReducerWithActionType<State, R>,
+): UseStoreWithState<State> | UseStoreWithReducer<State, R> {
   // keep references to all setters
   const setters: Dispatch<State>[] = [];
   let storeState: State = initialState;
 
-  // set new state and notify all setters about it
-  const setState = (
-    arg: [ReducerAction] extends [never] ? State | NewStateFn : ReducerAction,
-  ): void => {
-    let newState;
-
-    if (reducer instanceof Function) {
-      // reducer
-      newState = reducer(storeState, arg as ReducerAction);
-    } else if (arg instanceof Function) {
-      // state setting function
-      newState = arg(storeState);
-    } else {
-      // new state passed
-      newState = arg;
-    }
-
+  // set new state and notify all state consumers about it
+  const setState = (newState: State): void => {
     // do not do anything if state did not change
     if (newState === storeState) {
       return;
     }
 
     storeState = newState;
-    batch(() => setters.forEach((s: Dispatch<State>) => s(storeState)));
+    batch(() => {
+      const settersLength = setters.length;
+
+      for (let i = 0; i < settersLength; i++) {
+        setters[i](newState);
+      }
+    });
   };
 
-  return (): [State, typeof setState] => {
+  const setStateExternal =
+    reducer === undefined
+      ? (arg: State | ((oldState: State) => State)): void =>
+          setState(arg instanceof Function ? arg(storeState) : arg)
+      : (action: ReducerAction<State, R>): void =>
+          setState(reducer(storeState, action));
+
+  return (): [State, typeof setStateExternal] => {
     const [state, setter] = useState(storeState);
 
     useEffect(() => {
-      // keep track of new setters when component did mount
+      // keep track of new state consumers when component did mount
       if (setters.indexOf(setter) === -1) {
         setters.push(setter);
       }
 
       // returned function will run when component will unmount
-      return () => {
+      return (): void => {
         // remove setter from store
         const setterIdx = setters.indexOf(setter);
         setters.splice(setterIdx, 1);
       };
     }, [setter]); // variables that could cause a rerender
 
-    return [state, setState];
+    return [state, setStateExternal];
   };
 }
+
+export default makeStore;
