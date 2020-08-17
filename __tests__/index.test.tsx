58094aa06ec23a76537cfba99e1f15dcddf86441
pabra <patrick+github-commit-2020@braune.xyz>
2020-08-17 15:27:20 +0200

    move tests out of src dir

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, render, waitFor } from '@testing-library/react';
 import { act, renderHook } from '@testing-library/react-hooks';
 import React, { useEffect, useState } from 'react';
-import makeStore from '../index';
+import makeStore from '../src/index';
 
 test('should increment counter', () => {
   const useStore = makeStore(0);
